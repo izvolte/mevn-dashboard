d764d50bd74e7820e72666dfbeae5a84c442c976
izvolte <50668641+izvolte@users.noreply.github.com>
2021-01-12 20:40:26 +0300

    first commit

---
+++ components/Layouts/Nav.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <nav>
+        <h2>Menu</h2>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "footer"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    nav {
+        height: 100vh;
+        width: 20vw;
+        padding: 1rem;
+    }
+</style>
